54f8c8164e317ee6208032eb1a73c5c297efe17e
jyeong11 <187594303+jyeong11@users.noreply.github.com>
2025-05-11 16:06:01 +0900

    코테

--- CodeTest/src/Test/swea_1223.java
+++ CodeTest/src/Test/swea_1223.java
@@ -1,6 +1,7 @@
 package Test;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class swea_1223 {
 
@@ -16,10 +17,37 @@ public class swea_1223 {
 //
 //		[출력]
 //		#부호와 함께 테스트 케이스의 번호를 출력하고, 공백 문자 후 답을 출력한다.
-		Scanner scn = new Scanner(System.in);
-		int testLength = scn.nextInt();
-		
-
+		for(int j = 1; j <= 10; j++) {
+			Scanner scn = new Scanner(System.in);
+			scn.nextInt();
+			scn.nextLine();
+			String problem = scn.nextLine();
+			String[] arrs = problem.split("");
+			Stack<Integer> stack = new Stack<Integer>();
+			
+			for(int i = 0; i < arrs.length - 1; i++) {
+				if(arrs[i].equals("+")) {
+					String temp  = arrs[i];
+					arrs[i] = arrs[i + 1];
+					arrs[i + 1] = temp;
+				}
+			}
+			for(String arr : arrs) {
+//			arr을 String으로 받았으니 문자로 ["0~9"] 비교
+				if(arr.matches("[0-9]")) {
+					stack.push(Integer.parseInt(arr));
+				}else {
+					int c = stack.pop();
+					int d = stack.pop();
+//				문제에서는 +만 나오지만 /, *, - 의 경우를 생각해 switch문 사용
+					switch (arr) {
+					case "+":
+						stack.push(c + d);
+					}
+				}
+			}
+			System.out.println("#" + j + " " + stack.pop());
+		}
 	}
 
 }

c959b27d80452d4d6efaf31fad58079eb2a6341b
jyeong11 <bjy0911@naver.com>
2025-05-15 10:09:02 +0900

    2024/10/31

--- CodeTest/src/CodeUp_Test/codeup_1155.java
+++ CodeTest/src/CodeUp_Test/codeup_1155.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class codeup_1155 {
 
 	public static void main(String[] args) {
+//		인학이는 숫자 7을 좋아한다.
+//		어떤 정수가 입력되면 그 수가 7의 배수인지 확인하시오.
+		
 		Scanner scn = new Scanner(System.in);
 		int num = scn.nextInt();
 		

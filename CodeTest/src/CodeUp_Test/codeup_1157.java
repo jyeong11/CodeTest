58f39477d661ca97f8dcca595c791c4ccf082480
jyeong11 <bjy0911@naver.com>
2025-05-15 10:08:20 +0900

    2024/12/11

--- CodeTest/src/CodeUp_Test/codeup_1157.java
+++ CodeTest/src/CodeUp_Test/codeup_1157.java
@@ -8,7 +8,7 @@ public class codeup_1157 {
 //		공을 던져서 50m ~ 60m 사이에 공이 들어가면 슬기가 이기게 되고,
 //		그 외에 공이 떨어지면 체육선생님이 이기게 룰을 정했다.		
 //		슬기가 던진 공의 위치가 입력으로 주어지면 50이상 60이하이면 "win"을 출력하고,
-//		그 외에는 "lose"를 출력하시오
+//		그 외에는 "lose"를 출력하시오.
 		Scanner scn = new Scanner(System.in);
 		double num = scn.nextDouble();
 		

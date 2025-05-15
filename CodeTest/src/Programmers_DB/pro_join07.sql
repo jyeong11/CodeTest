aa5fab33479d81a96e8babac4c37ddbb9a13093f
jyeong11 <bjy0911@naver.com>
2025-05-15 12:36:56 +0900

    프로그래머스 join07

---
+++ CodeTest/src/Programmers_DB/pro_join07.sql
@@ -0,0 +1,7 @@
+select animal_id, name
+  from animal_ins ai
+ where exists (select 1
+                 from animal_outs ao
+                where ai.animal_id = ao.animal_id
+                  and ai.datetime > ao.datetime)
+ order by ai.datetime;

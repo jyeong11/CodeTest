d63f4fa343d212e4bf7b3d07399f748e8f64ea01
jyeong11 <bjy0911@naver.com>
2025-05-15 09:03:25 +0900

    2024-10-31

---
+++ CodeTest/src/Test/codeup_1155.java
@@ -0,0 +1,17 @@
+package Test;
+
+import java.util.Scanner;
+
+public class codeup_1155 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int num = scn.nextInt();
+		
+		if(num % 7 ==0) {
+			System.out.println("multiple");
+		}else {
+			System.out.println("not multiple");
+		}
+	}
+}

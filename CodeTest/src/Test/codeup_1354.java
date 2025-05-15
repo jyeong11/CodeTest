99179ab576db0243c504e725f1c90338df39ed7f
jyeong11 <bjy0911@naver.com>
2025-05-15 09:07:57 +0900

    2024-11-05

--- CodeTest/src/Test/codeup_1354.java
+++ CodeTest/src/Test/codeup_1354.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class codeup_1354 {
 	
 	public static void main(String[] args) {
+//		길이 n이 입력되면 역삼각형을 출력한다.
+//		예) n이 5이면
+//		*****
+//		****
+//		***
+//		**
+//		*
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
 	

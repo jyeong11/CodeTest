930978544c2a47cd5f57db5bc2e4c7526e99830c
jyeong11 <187594303+jyeong11@users.noreply.github.com>
2025-05-25 21:05:22 +0900

    프로그래머스 join04번

--- CodeTest/src/Programmers_DB/pro_join04.sql
+++ CodeTest/src/Programmers_DB/pro_join04.sql
@@ -1,6 +1,15 @@
+-- 첫번쨰 MySQL
 select b.book_id, a.author_name, date_format(b.published_date,'%Y-%m-%d')
   from book b 
   join author a
     on b.author_id = a.author_id
  where b.category = '경제'
  order by b.published_date;
+ 
+-- 두번째 Oracle
+ select b.book_id, a.author_name, TO_CHAR(b.published_date, 'YYYY-MM-DD') as published_date
+  from book b
+  join author a
+    on b.author_id = a.author_id
+ where b.category = '경제' 
+ order by published_date;
